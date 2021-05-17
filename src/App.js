ed0578426c69bf0ac87d5655fb395be93ad5c96a
AdarshNaidu <naidu.adarsh2511@gmail.com>
2021-05-17 22:42:46 +0530

    Displaying notes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
-import React from 'react'
+import React, {useEffect} from 'react'
+import Notes from './components/Notes'
+import { initializeNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(initializeNotes()) 
+  },[dispatch]) 
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Notes />
+    </div>
   )
 }
 
